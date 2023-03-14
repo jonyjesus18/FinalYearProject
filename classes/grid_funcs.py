18b8f1b9b46e0b3e5da43efd9c34ed82989b06c9
João Jesus <joaojesus@Joaos-MacBook-Air.local>
2023-03-14 11:33:15 +0000

    adding support for two types of coordinates

--- classes/grid_funcs.py
+++ classes/grid_funcs.py
@@ -5,12 +5,12 @@ class map_grid():
     Map_grid creates a grid object of coordinates to simply selection of geographical areas.
 
     '''
-    def __init__(self):
-        
-        self.nh_lat = np.genfromtxt('Airs_nh_lat.csv', delimiter=',')
-        self.nh_lon = np.genfromtxt('Airs_nh_lon.csv', delimiter=',')
-        self.sh_lat = np.genfromtxt('Airs_sh_lat.csv', delimiter=',')
-        self.sh_lon = np.genfromtxt('Airs_sh_lon.csv', delimiter=',')
+    def __init__(self,grid_size):
+        self.grid_size  = grid_size
+        self.nh_lat = np.genfromtxt(f'COORDINATES_{grid_size}KM_GRID/Airs_nh_lat_{grid_size}_grid.csv', delimiter=',')
+        self.nh_lon = np.genfromtxt(f'COORDINATES_{grid_size}KM_GRID/Airs_nh_lon_{grid_size}_grid.csv', delimiter=',')
+        self.sh_lat = np.genfromtxt(f'COORDINATES_{grid_size}KM_GRID/Airs_sh_lat_{grid_size}_grid.csv', delimiter=',')
+        self.sh_lon = np.genfromtxt(f'COORDINATES_{grid_size}KM_GRID/Airs_sh_lon_{grid_size}_grid.csv', delimiter=',')
 
         self.nh_stacked = np.stack((self.nh_lat, self.nh_lon), axis=2)
         self.sh_stacked = np.stack((self.sh_lat, self.sh_lon), axis=2)
@@ -18,7 +18,7 @@ class map_grid():
     def select_area_indexer(self, min_lat,max_lat,min_lon,max_lon):
         '''
         Selects coordinates for a range of longitudes and latitudes.
-        Output is an array of indeces of shape (501,501) used to filter the output from  mamatlab_reader.select()
+        Output is an array of indeces of shape (n,n) used to filter the output from  mamatlab_reader.select()
         '''
         if max_lat < 0:
             a = self.sh_stacked
@@ -29,7 +29,7 @@ class map_grid():
 
 class dataset():
     def __init__(self,data):
-        ''' Data class : holds an array of data of shape 501x501'''
+        ''' Data class : holds an array of data of shape nxn'''
         self.data = data
         self.mean = np.nanmean(data)
         self.max = np.nanmax(data)
@@ -40,6 +40,6 @@ class data_wizard():
     '''Colletion of functions to simplify filtering and agreggating geospatial matlab data'''
     def data_filtering(self,select_area_indexer,data):
         temp_data_select = data[select_area_indexer[:,0], select_area_indexer[:,1]]
-        mask = np.full((501, 501), np.nan)
+        mask = np.full(np.shape(data), np.nan)
         mask[select_area_indexer[:,0], select_area_indexer[:,1]] = temp_data_select
         return mask
